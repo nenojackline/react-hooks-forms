ee19e866a6e1bda2d27a8ce26aad80f3b92c43a1
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-15 16:57:56 +0300

    Done

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,19 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+//we're updating state based on event.target.value
+function handleFirstNameChange(event) {
+  setFirstName(event.target.value);
+}
+
+function handleLastNameChange(event) {
+  setLastName(event.target.value);
+}
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
